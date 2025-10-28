0b67db7ad1ce21fb779a9e691df6f9547512508f
Manoj0919 <kummaramanojkumarr@gmail.com>
2025-10-28 21:56:00 +0530

    m

--- src/main/java/com/rushi/controller/HomeController.java
+++ src/main/java/com/rushi/controller/HomeController.java
@@ -91,6 +91,6 @@ public class HomeController {
     }
 
     private List<String> getCourses() {
-        return Arrays.asList("DevOps", "AWS", "Azure", "Terraform", "Kubernetes","Python","linux","dataE");
+        return Arrays.asList("DevOps", "AWS", "Azure", "Terraform", "Kubernetes","Python","linux","dataEn");
     }
 }
