f55a80cc521e8242f2e886227abeffd7a4ff2f6e
Rushi-Technologies <your.email@example.com>
2026-06-01 21:16:10 +0530

    Fixed Code Quality Issues

--- src/main/java/com/rushi/controller/HomeController.java
+++ src/main/java/com/rushi/controller/HomeController.java
@@ -12,7 +12,6 @@ import java.io.Serializable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
